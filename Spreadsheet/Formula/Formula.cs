a0072beb770afcb7175420020d5ee8fa79d8a9db
Morgan Empey <morgan.empey@gmail.com>
2016-01-23 13:57:39 -0700

    Completed implementation of Formula constructor.

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -48,24 +48,84 @@ namespace Formulas
             formulaString = formula;
 
             // Store formula in container as tokens
-            List<string> formulaTokens = GetTokens(formula).ToList<string>();
+            //List<string> formulaTokens = GetTokens(formula).ToList<string>();
 
             // Check that there is at least one token
-            if (formulaTokens.Count == 0)
-            {
-                throw new FormulaFormatException("Empty formula");
-            }
+            //if (formulaTokens.Count == 0)
+            //{
+            //    throw new FormulaFormatException("Empty formula");
+            //}
+
+            int tokenCount = 0;
+            int openParenCount = 0;
+            int closeParentCount = 0;
+            string prevToken = "";
               
-            // Check for invalid syntax
+            // Check for invalid tokens or syntax
             foreach (string token in GetTokens(formula))
             {
+                tokenCount++;
+
+                if (token == "(")
+                {
+                    openParenCount++;
+                }
+                else if (token == ")")
+                {
+                    closeParentCount++;
+                }
+
                 if (!IsValidToken(token))
                 {
                     throw new FormulaFormatException("Invalid token");
                 }
+
+                if (tokenCount == 1 && !IsNumberToken(token) && !IsVariableToken(token) && token != "(")
+                {
+                    throw new FormulaFormatException("First token must be a number, variable, or open parentheses");
+                }
+
+                if (closeParentCount > openParenCount)
+                {
+                    throw new FormulaFormatException("Excess close parentheses");
+                }
+
+                if (prevToken == "(" || IsOperatorToken(prevToken))
+                {
+                    if (!IsNumberToken(token) && !IsVariableToken(token) && token != "(")
+                    {
+                        throw new FormulaFormatException("Invalid syntax"); // TODO better message here?
+                    }
+                }
+
+                if (IsNumberToken(prevToken) || IsVariableToken(prevToken) || prevToken == ")")
+                {
+                    if (!IsOperatorToken(token) && token != ")")
+                    {
+                        throw new FormulaFormatException("Invalid syntax"); // TODO better message here?
+                    }
+                }
+
+                prevToken = token;
+            }
+
+            if (tokenCount == 0)
+            {
+                throw new FormulaFormatException("Empty formula");
+            }
+
+            if (openParenCount != closeParentCount)
+            {
+                throw new FormulaFormatException("Parentheses count unequal");
+            }
+
+            if (!IsNumberToken(prevToken) && !IsVariableToken(prevToken) && prevToken != ")")
+            {
+                throw new FormulaFormatException("Last token must be a number, variable, or close parentheses");
             }
             
         }
+
         /// <summary>
         /// Evaluates this Formula, using the Lookup delegate to determine the values of variables.  (The
         /// delegate takes a variable name as a parameter and returns its value (if it has one) or throws
@@ -77,6 +137,15 @@ namespace Formulas
         /// </summary>
         public double Evaluate(Lookup lookup)
         {
+            // Create value and operator stacks
+            var operatorStack = new Stack<string>();
+            var valueStack = new Stack<double>();
+
+            foreach (string token in GetTokens(formulaString))
+            {
+
+            }
+
             return 0;
         }
 
@@ -114,33 +183,34 @@ namespace Formulas
         /// Returns true if the token is a valid formula token.  Valid tokens are:
         ///     -Open or close parentheses
         ///     -+, -, *, or /
-        ///     -Non-negative numbers
+        ///     -Floating-point numbers
         ///     -Variables, which consist of one or more letters followed by one or more numbers
         /// </summary>
         /// <param name="token"></param>
         /// <returns></returns>
         private Boolean IsValidToken(string token)
         {
-            if (token.Equals("(") || token.Equals(")") ||
-                token.Equals("+") || token.Equals("-") ||
-                token.Equals("*") || token.Equals("/"))
-            {
-                return true;
-            }
-
-            double d;
-
-            if (Double.TryParse(token, out d))
-            {
-                return true;
-            }
-
-            if (IsVariableToken(token))
+            if (token.Equals("(") ||
+                token.Equals(")") ||
+                IsOperatorToken(token) ||
+                IsNumberToken(token) ||
+                IsVariableToken(token))
             {
                 return true;
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Returns true if the token is a valid floating-point number.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        private Boolean IsNumberToken(string token)
+        {
+            double d;
+            return Double.TryParse(token, out d);
         }
 
         /// <summary>
@@ -150,7 +220,7 @@ namespace Formulas
         /// <returns></returns>
         private Boolean IsVariableToken(string token)
         {
-            if (!IsLetter(token[0]))
+            if (token == "" || !IsLetter(token[0]))
             {
                 return false;
             }
@@ -164,6 +234,21 @@ namespace Formulas
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Returns true if the token is a valid operator (+, -, *, or /)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private Boolean IsOperatorToken(string s)
+        {
+            if (s == "+" || s == "-" || s == "*" || s == "/")
+            {
+                return true;
+            }
+
+            return false;
         }
 
         /// <summary>
@@ -195,7 +280,6 @@ namespace Formulas
 
             return false;
         }
-
         
     }
 
