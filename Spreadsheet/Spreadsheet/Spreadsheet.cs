7e3ae7d5d17e5314d50f71015949b659f5414825
Morgan Empey <morgan.empey@gmail.com>
2016-02-13 13:10:07 -0700

    Completed skeleton for Spreadsheet class.

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -7,8 +7,44 @@ using Formulas;
 
 namespace SS
 {
+    /// <summary>
+    /// A Spreadsheet object represents the state of a simple spreadsheet.  A 
+    /// spreadsheet consists of an infinite number of named cells.
+    /// 
+    /// A string is a cell name if and only if it consists of one or more letters, 
+    /// followed by a non-zero digit, followed by zero or more digits.  Cell names
+    /// are not case sensitive.
+    /// 
+    /// For example, "A15", "a15", "XY32", and "BC7" are cell names.  (Note that 
+    /// "A15" and "a15" name the same cell.)  On the other hand, "Z", "X07", and 
+    /// "hello" are not cell names."
+    /// 
+    /// A spreadsheet contains a cell corresponding to every possible cell name.  
+    /// In addition to a name, each cell has a contents and a value.  The distinction is
+    /// important, and it is important that you understand the distinction and use
+    /// the right term when writing code, writing comments, and asking questions.
+    /// 
+    /// Spreadsheets are never allowed to contain a combination of Formulas that establish
+    /// a circular dependency.  A circular dependency exists when a cell depends on itself.
+    /// For example, suppose that A1 contains B1*2, B1 contains C1*2, and C1 contains A1*2.
+    /// A1 depends on B1, which depends on C1, which depends on A1.  That's a circular
+    /// dependency.
+    /// </summary>
     class Spreadsheet : AbstractSpreadsheet
     {
+        /// <summary>
+        /// A Spreadsheet is set of Cells.
+        /// </summary>
+        private HashSet<Cell> cells;
+
+        /// <summary>
+        /// Creates an empty Spreadsheet
+        /// </summary>
+        public Spreadsheet()
+        {
+            cells = new HashSet<Cell>();
+        }
+
         /// <summary>
         /// If name is null or invalid, throws an InvalidNameException.
         /// 
@@ -100,6 +136,66 @@ namespace SS
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// A spreadsheet contains a cell corresponding to every possible cell name.  
+        /// In addition to a name, each cell has a contents and a value.
+        /// </summary>
+        private class Cell
+        {
+            /// <summary>
+            /// A string is a cell name if and only if it consists of one or more letters, 
+            /// followed by a non-zero digit, followed by zero or more digits.  Cell names
+            /// are not case sensitive.
+            /// </summary>
+            private string name;
+
+            /// <summary>
+            /// The contents of a cell can be (1) a string, (2) a double, or (3) a Formula.  If the
+            /// contents is an empty string, we say that the cell is empty.  (By analogy, the contents
+            /// of a cell in Excel is what is displayed on the editing line when the cell is selected.)
+            /// 
+            /// In an empty spreadsheet, the contents of every cell is the empty string.
+            ///  
+            /// If a cell's contents is a string, its value is that string.
+            /// 
+            /// If a cell's contents is a double, its value is that double.
+            /// 
+            /// If a cell's contents is a Formula, its value is either a double or a FormulaError.
+            /// The value of a Formula, of course, can depend on the values of variables.  The value 
+            /// of a Formula variable is the value of the spreadsheet cell it names (if that cell's 
+            /// value is a double) or is undefined (otherwise).  If a Formula depends on an undefined
+            /// variable or on a division by zero, its value is a FormulaError.  Otherwise, its value
+            /// is a double, as specified in Formula.Evaluate.
+            /// </summary>
+            private object contents;
+
+            /// <summary>
+            /// The value of a cell can be (1) a string, (2) a double, or (3) a FormulaError.  
+            /// (By analogy, the value of an Excel cell is what is displayed in that cell's position
+            /// in the grid.)
+            /// </summary>
+            private object value;
+
+            /// <summary>
+            /// Creates an empty cell.
+            /// </summary>
+            private Cell(string name)
+            {
+                this.name = name;
+                contents = "";
+                value = "";
+            }
+
+            /// <summary>
+            /// Returns the hash code for this Cell
+            /// </summary>
+            public override int GetHashCode()
+            {
+                return name.GetHashCode();
+            }
+
         }
     }
 }
