c4487ee72e1f925fa5e4ec563722f33d4654103b
Morgan Empey <morgan.empey@gmail.com>
2016-02-27 13:53:09 -0700

    Defined ISpreadsheetView interface

---
+++ Spreadsheet/SpreadsheetGUI/ISpreadsheetView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SpreadsheetGUI
+{
+    // Provides a controllable interface for the SpreadsheetWindow
+    public interface ISpreadsheetView
+    {
+        event Action NewFileEvent;
+
+        event Action<string> FileChosenEvent;
+
+        event Action<string> SaveFileEvent;
+
+        event Action CloseEvent;
+
+        event Action HelpContentsEvent;
+
+        event Action<string> SetContentsEvent;
+
+        event Action<string> CellSelectionChangedEvent;
+
+        void SetCellValue(int col, int row, string value);
+
+        string WindowName { set; }
+
+        string SelectedCellName { get; set; }
+
+        string SelectedCellValue { set; }
+
+        string SelectedCellContents { get; set; }
+    }
+}
