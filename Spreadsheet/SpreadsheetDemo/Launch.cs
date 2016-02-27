53833a462bdddc4ec76b69ac5602e6c7e86ca3ec
Morgan Empey <morgan.empey@gmail.com>
2016-02-27 10:47:16 -0700

    Revert "Fixed compiler error in SpreadsheetDemo Launch.cs"
    This reverts commit c73fed26afd71f40974fcef90124a450fef60a05.

--- Spreadsheet/SpreadsheetDemo/Launch.cs
+++ Spreadsheet/SpreadsheetDemo/Launch.cs
@@ -17,7 +17,7 @@ namespace SS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SSGui.SpreadsheetDemo());
+            Application.Run(new SpreadsheetDemo());
         }
     }
 }
